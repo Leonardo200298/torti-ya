f027a57afd41696ebec920e8291f599d6b4a3f1c
Gonzalo Salomon <salomongonzalo8@gmail.com>
2022-12-22 18:39:46 -0300

    finishing third commit
    todavía quedaron por cargar las variables de entorno, pero se avanzó

--- app/page.jsx
+++ app/page.jsx
@@ -1,17 +1,23 @@
 "use client"
 import { React, useState, useEffect } from 'react'
-import { Image } from 'next/image'
+import { createClient } from '@supabase/supabase-js'
 
-const fetchProducts = async () => {
-    /*poner la data que traemos desde supabase/productos*/
+async function fetchItems(){
+    const supabaseUrl = process.env.API_URL
+    const supabaseKey = process.env.API_KEY
+    console.log('La URL es:'+supabaseUrl+' y la clave es:'+supabaseKey)
+    const supabase = createClient(supabaseUrl, supabaseKey)
 }
 
 export default function Page() {
     const [items, setItems] = useState([])
 
     useEffect(()=>{
-        setItems(fetchProducts)
+        fetchItems()
+        //fetchItems().map((item)=>setItems([...items, item]))
     },[])
+
+    console.log(items)
 
     return (
         <section>
